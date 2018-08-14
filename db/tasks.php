6f9c3c7aceb7ae985ceac748822628bc50035d74
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2018-08-14 17:52:41 -0400

    Fix tasks.php and add missing language string

--- db/tasks.php
+++ db/tasks.php
@@ -4,7 +4,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $tasks = array(
     array(
-        'classname' => 'mod_wwassignment\task\updatedirtysets',
+        'classname' => 'mod_wwassignment\task\update_dirty_sets',
         'blocking' => 0,
         'minute' => 'R',
         'hour' => '4',
